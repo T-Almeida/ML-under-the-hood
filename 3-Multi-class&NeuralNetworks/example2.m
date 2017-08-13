e8ada608c0c9088d444813c4dab67b4f4e05017c
tiago alexandre melo almeida <tiagomeloalmeida@ua.pt>
2017-08-13 22:40:55 +0100

    Neural Network and more multi class

---
+++ 3-Multi-class&NeuralNetworks/example2.m
@@ -0,0 +1,81 @@
+%Same as ex3_nn but show the errors
+
+% using some function from other folder
+addpath("../1-Linear&PolynomialRegression");
+
+%% Initialization
+clear ; close all; clc
+
+%% Setup the parameters you will use for this exercise
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+hidden_layer_size = 25;   % 25 hidden units
+num_labels = 10;          % 10 labels, from 1 to 10   
+                          % (note that we have mapped "0" to label 10)
+
+%% =========== Part 1: Loading and Visualizing Data =============
+%  We start the exercise by first loading and visualizing the dataset. 
+%  You will be working with a dataset that contains handwritten digits.
+%
+
+% Load Training Data
+fprintf('Loading and Visualizing Data ...\n')
+
+load('ex3data1.mat');
+m = size(X, 1);
+
+% Randomly select 100 data points to display
+sel = randperm(size(X, 1));
+sel = sel(1:100);
+
+displayData(X(sel, :));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ================ Part 2: Loading Pameters ================
+% In this part of the exercise, we load some pre-initialized 
+% neural network parameters.
+
+fprintf('\nLoading Saved Neural Network Parameters ...\n')
+
+% Load the weights into variables Theta1 and Theta2
+load('ex3weights.mat');
+
+%% ================= Part 3: Implement Predict =================
+%  After training the neural network, we would like to use it to predict
+%  the labels. You will now implement the "predict" function to use the
+%  neural network to predict the labels of the training set. This lets
+%  you compute the training set accuracy.
+
+pred = predict(Theta1, Theta2, X);
+
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ================ Part 4: Show some erros ================
+
+fail = ~(pred == y);
+X_fail = X(fail,:);
+
+for i = 1:size(X_fail,1)
+    % Display 
+    fprintf('\nDisplaying Example Image\n');
+    displayData(X_fail(i, :));
+
+    pred = predict(Theta1, Theta2, X(rp(i),:));
+    fprintf('\nNeural Network Prediction: digit %d\n', mod(pred, 10));
+    
+    % Pause with quit option
+    s = input('Paused - press enter to continue, q to exit:','s');
+    if s == 'q'
+      break
+    end
+end
+
+fprintf('All errors.\n');
+displayData(X_fail);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
